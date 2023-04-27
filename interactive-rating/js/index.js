9289dd2b8f2e0c3d45208f8e18766f6dc424b1b8
Wagner Ricardo de Paula Marcolongo <122945519+WagnerRicardo@users.noreply.github.com>
2023-04-26 23:22:54 -0300

    project finalization

--- interactive-rating/js/index.js
+++ interactive-rating/js/index.js
@@ -1,13 +1,20 @@
 function onsub(event){
     event.preventDefault()
     var rating = form['rating'].value
-    console.log(rating)
-    location.href = 'thanks.html'
+    if (rating != 0){
+        console.log(rating)
+        sessionStorage.setItem("rate", rating);
+        location.href = 'thanks.html'    
+    }
 }
-
-var star
-
+//adding an event on submit if a form exists
 if (document.forms[0]){
     var form = document.forms['form']
     form.addEventListener("submit", onsub) 
+}
+//displaying the rating on the thanks page
+if(document.getElementById('rateValue')){
+    let span = document.getElementById('rateValue')
+    let rate = sessionStorage.getItem('rate')
+    span.innerHTML = rate
 }
